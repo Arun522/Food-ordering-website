6ae32558e8ebe2f9bf44fae30c80fe5365568c67
Arunkumar Velu <mailarun522@gmail.com>
2023-11-17 23:44:22 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="max-w-[1640px] m-auto px-4 pt-8">
       <div className="bg-gray-200 w-full flex items-center justify-center py-3 h-20">
         <p className="text-black text-center text-sm md:text-base">
-          Copyright &copy; Ebuka Eats by Uncle James 2023. All rights reserved.
+          Copyright &copy; Arun Eats by Uncle Anand 2023. All rights reserved.
         </p>
       </div>
     </div>
